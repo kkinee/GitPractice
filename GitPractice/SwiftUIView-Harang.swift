a2f3323f0f1a76863eae7d86638cb3d4f7ddda06
이준후 <hprize04@gmail.com>
2023-05-10 11:53:37 +0900

    Harang push

---
+++ GitPractice/SwiftUIView-Harang.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView-Harang.swift
+//  GitPractice
+//
+//  Created by 이준후 on 2023/05/10.
+//
+
+import SwiftUI
+
+struct SwiftUIView_Harang: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Harang_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView_Harang()
+    }
+}
